cf3b1a838bd17e5e0d9245dde34cbcead78d97f3
薛翔 <xuexiangjys@163.com>
2018-08-08 23:58:45 +0800

    解决CacheAndRemoteDistinctStrategy缓存策略的bug

--- xhttp2-lib/src/main/java/com/xuexiang/xhttp2/cache/stategy/CacheAndRemoteDistinctStrategy.java
+++ xhttp2-lib/src/main/java/com/xuexiang/xhttp2/cache/stategy/CacheAndRemoteDistinctStrategy.java
@@ -16,6 +16,7 @@
 
 package com.xuexiang.xhttp2.cache.stategy;
 
+import com.google.gson.Gson;
 import com.xuexiang.xhttp2.cache.RxCache;
 import com.xuexiang.xhttp2.cache.model.CacheResult;
 
@@ -28,7 +29,11 @@ import io.reactivex.functions.Predicate;
 import okio.ByteString;
 
 /**
- * <p>描述：先显示缓存，再请求网络</p>
+ * <p>描述:
+ * 先使用缓存，不管是否存在，仍然请求网络，会先把缓存回调给你，
+ * 等网络请求回来发现数据是一样的就不会再返回，否则再返回
+ * （这样做的目的是防止数据是一样的你也需要刷新界面）
+ * </p>
  * <-------此类加载用的是反射 所以类名是灰色的 没有直接引用  不要误删----------------><br>
  *
  * @author xuexiang
@@ -38,18 +43,18 @@ public final class CacheAndRemoteDistinctStrategy extends BaseStrategy {
 
     @Override
     public <T> Observable<CacheResult<T>> execute(RxCache rxCache, String key, long time, Observable<T> source, Type type) {
-        Observable<CacheResult<T>> cache = loadCache(rxCache, type, key, time,true);
-        Observable<CacheResult<T>> remote = loadRemote(rxCache, key, source,false);
+        Observable<CacheResult<T>> cache = loadCache(rxCache, type, key, time, true);
+        Observable<CacheResult<T>> remote = loadRemote(rxCache, key, source, false);
         return Observable.concat(cache, remote)
                 .filter(new Predicate<CacheResult<T>>() {
                     @Override
                     public boolean test(@NonNull CacheResult<T> tCacheResult) throws Exception {
                         return tCacheResult != null && tCacheResult.data != null;
                     }
-                }).distinctUntilChanged(new Function<CacheResult<T>, String>() {
+                }).distinct(new Function<CacheResult<T>, String>() {
                     @Override
                     public String apply(@NonNull CacheResult<T> tCacheResult) throws Exception {
-                        return  ByteString.of(tCacheResult.data.toString().getBytes()).md5().hex();
+                        return ByteString.encodeUtf8(new Gson().toJson(tCacheResult.data)).md5().hex();
                     }
                 });
     }
